42d2dc67cc9642d0bf0aec73fb782cbdced316c2
CarlitaS630 <carlasalinas630@gmail.com>
2025-10-23 23:12:26 +0200

    Controlador de categorias listo

--- Controller/categoriaControlador.php
+++ Controller/categoriaControlador.php
@@ -1,9 +1,12 @@
 <?php
+use Laminas\Diactoros\Response\JsonResponse;
+
 require_once __DIR__ . '/../Models/Categorias.php';
-Class CategoriaControlador{
+Class CategoriasController{
 
     public function mostrar(){
-
+        $catergorias=new Categorias;
+        return new JsonResponse($catergorias->get());
     }
 
 }
---
+++ Models/Categorias.php
@@ -0,0 +1,8 @@
+<?php
+class Categorias{
+    public function get()
+    {
+
+    }
+}
+?>
